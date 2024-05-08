b7ce56fd8ae10dd5c8fc5cc0e26aa77cba47b088
LloydElery <dennisgooglar@gmail.com>
2024-05-08 16:57:44 +0200

    Add wörks

--- test-express/src/controllers/cardController.ts
+++ test-express/src/controllers/cardController.ts
@@ -3,16 +3,20 @@ import Card from "../models/cardModel";
 const create = async (data: any) => {
   return await Card.create(data);
 };
+
 const readAll = async () => {
   const Cards = await Card.find({}).limit(5);
   return Cards;
 };
+
 const read = async (id: any) => {
   return await Card.findById(id);
 };
+
 const update = async (id: any, data: any) => {
   return await Card.findByIdAndUpdate(id, data, { new: true });
 };
+
 const deleteOne = async (id: any) => {
   return await Card.findByIdAndDelete(id);
 };
@@ -21,19 +25,22 @@ export const getAllCards = async (req: any, res: any) => {
   const card = await readAll();
   res.status(200).json(card);
 };
+
 export const addCard = async (req: any, res: any) => {
   const { name } = req.body;
 
-  const result = await create({});
+  const result = await create({ name });
 
-  res.status(201).json({ message: "card created successfully", card: result });
+  res.status(201).json({ message: "Card created successfully", card: result });
 };
+
 export const getCard = async (req: any, res: any) => {
   const id = req.params.id;
   const card = await read(id);
 
   res.status(200).json(card);
 };
+
 export const updateCard = async (req: any, res: any) => {
   const { title, author, publisher, read } = req.body;
   const id = req.params.id;
@@ -44,6 +51,7 @@ export const updateCard = async (req: any, res: any) => {
     .status(200)
     .json({ message: "Update succeeded", updatedcard: updatedcard });
 };
+
 export const deleteCard = async (req: any, res: any) => {
   const id = req.params.id;
 
