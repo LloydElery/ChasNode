85060ab41ef833b384f50b7b6ffce91bd8741a86
LloydElery <dennisgooglar@gmail.com>
2024-05-16 15:09:03 +0200

    Typescript socket

---
+++ websocket/ts-sockets/index.ts
@@ -0,0 +1,24 @@
+import express from "express";
+const app = express();
+import http from "http";
+const server = http.createServer(app);
+import { Server } from "socket.io";
+const io = new Server(server);
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+io.on("connection", (socket) => {
+  console.log("A user connected");
+  socket.on("chat message", (msg) => {
+    io.emit("chat message", msg);
+  });
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+});
+
+server.listen(3000, () => {
+  console.log("listening on http://localhost:3000");
+});
